93e449cba6fb519adf221f79e7a129a9266a2688
jane <webdjanelee@gmail.com>
2020-12-06 18:57:02 +0900

    add_Protect routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = null;
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-
-      <Route exact path="/signin">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+      >
         <Signin />
-      </Route>
+      </IsUserRedirect>
 
-      <Route exact path="/signup">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+      >
         <Signup />
-      </Route>
+      </IsUserRedirect>
 
-      <Route exact path={ROUTES.HOME}>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,5 @@
+import { useState, useEffect, useContext } from "react";
+
+import { FirebaseContext } from "../context/firebase";
+
+export default function useAuthListener() {}
